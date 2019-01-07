a2ab38b28f2a97e8598f116d0e91d154f0363476
Facundo Palombo <facundo.palombo@gmail.com>
2019-01-07 09:18:53 -0300

    Se arreglan errores de nomenclatura de archivos y se agrega las rutas desde el index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 const express = require("express")
 const app = express()
 const port = process.env.port || 3000;
-
+const {router} = require("./routes/router")
+//Routes
+app.use("/" , router);
 
 //Server initialized on port 3000
 app.listen(port , ()=>{
