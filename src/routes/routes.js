7cedcffc3910c729e2fb0ca427ba692d4e64827e
Facundo Palombo <facundo.palombo@gmail.com>
2019-01-07 09:10:46 -0300

    Se crean rutas de about y who

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,3 +4,11 @@ const router = express.Router()
 router.get("/", (req,res)=>{
     res.send("Esta solicitando la pagina principal!")
 })
+
+router.get("/about", (req,res)=>{
+    res.send("Esta solicitando la pagina ABOUT!")
+})
+
+router.get("/who", (req,res)=>{
+    res.send("Esta solicitando la pagina de WHO WE ARE!")
+})
